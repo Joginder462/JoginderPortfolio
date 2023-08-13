7e80d0119568215b7312c9cd915b86164d4fbf8c
Joginder462 <138439431+Joginder462@users.noreply.github.com>
2023-08-13 06:01:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,9 @@
+let x = document.getElementsByClassName("bottom")[0];
+
+function myFunction() {
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
