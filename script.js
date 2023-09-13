03904e8b995b669c8e23b574e277bf4e84a0f3f1
Joginder462 <138439431+Joginder462@users.noreply.github.com>
2023-09-13 11:02:00 +0530

    deleterd old

--- script.js
+++
@@ -1,9 +0,0 @@
-let x = document.getElementsByClassName("bottom")[0];
-
-function myFunction() {
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
